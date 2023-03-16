eb4f55577036ebdda2b345a59189da19d8d2899f
Ingridh Oliveira <ingridhnunes@gmail.com>
2023-03-15 23:00:20 -0300

    axios and endpoint

--- src/components/ResultCard/index.tsx
+++ src/components/ResultCard/index.tsx
@@ -1,21 +1,27 @@
 import EmptyImage from "../../assets/images/vector.jpg";
 import "./style.css";
 
-const ResultCard = () => {
-  type User = {
-    avatar_url: string;
-    url: string;
-    followers_url: string;
-    location: string;
-    name: string;
-  };
+interface ResultCardProps {
+  avatar: string;
+  url: string;
+  followers: string;
+  location: string;
+  name: string;
+}
 
+const ResultCard = ({
+  avatar,
+  url,
+  followers,
+  location,
+  name,
+}: ResultCardProps) => {
   return (
     <>
       <div className="container main-result-container">
         <div className="container result-container">
           <div className="empty-image-container">
-            <img alt="none" src={EmptyImage} />
+            <img alt="none" src={avatar} />
           </div>
           <div className="information-container container">
             <h4 className="text-primary">Informações</h4>
@@ -34,16 +40,16 @@ const ResultCard = () => {
                       aria-describedby="basic-addon1"
                     />
                   </div> */}
-                  <b>Perfil:</b> https://api.github.com/users/ingridholiveira
+                  <b>Perfil:</b> {url}
                 </li>
                 <li className="list-group-item">
-                  <b>Seguidores:</b> 3
+                  <b>Seguidores:</b> {followers}
                 </li>
                 <li className="list-group-item">
-                  <b>Localidade:</b> Itapetininga
+                  <b>Localidade:</b> {location}
                 </li>
                 <li className="list-group-item">
-                  <b>Nome:</b> Ingridh Nunes Lima de Oliveira
+                  <b>Nome:</b> {name}
                 </li>
               </ul>
             </div>
--- src/pages/Result/index.tsx
+++ src/pages/Result/index.tsx
@@ -1,19 +1,58 @@
+import axios from "axios";
+import { useState } from "react";
 import ResultCard from "../../components/ResultCard";
 import "./style.css";
 
+type FormData = {
+  name: string;
+};
+
+type User = {
+  avatar_url: string;
+  url: string;
+  followers: string;
+  location: string;
+  name: string;
+};
+
 const Result = () => {
+  const [formData, setFormData] = useState<FormData>({
+    name: "",
+  });
+  const [user, setUser] = useState<User>();
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    setFormData({ ...formData, [name]: value });
+  };
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    axios
+      .get(`https://api.github.com/users/${formData.name}`)
+      .then((response) => {
+        setUser(response.data);
+      })
+      .catch((err) => {
+        setUser(undefined);
+      });
+  };
+
   return (
     <>
       <div className="main-search-container">
         <div className="container search-container">
           <h1>Encontre um perfil Github</h1>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="form-container">
               <input
                 type="text"
                 name="name"
+                value={formData.name}
                 className="search-input"
                 placeholder="Usuário Github"
+                onChange={handleChange}
               />
 
               <button type="submit" className="btn btn-primary search-button">
@@ -22,7 +61,16 @@ const Result = () => {
             </div>
           </form>
         </div>
-        <ResultCard />
+
+        {user && (
+          <ResultCard
+            avatar={user.avatar_url}
+            url={user.url}
+            followers={user.followers}
+            location={user.location}
+            name={user.name}
+          />
+        )}
       </div>
     </>
   );
